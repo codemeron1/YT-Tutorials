714f17e1e304a36c833235ad86dc24f1eeda1314
Ron <wondelrosario@gmail.com>
2021-07-30 11:37:44 +0800

    Add WebView2 DOM Manipulation using WinForm C#.

---
+++ CSharp/WebView2Demo/WebView2Demo/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WebView2Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView
+            // 
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Location = new System.Drawing.Point(27, 50);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(273, 467);
+            this.webView.TabIndex = 0;
+            this.webView.ZoomFactor = 1D;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(342, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Change Font Color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(342, 260);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(211, 47);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Change Header Text";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 578);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ CSharp/WebView2Demo/WebView2Demo/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+using System.IO;
+
+namespace WebView2Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitWebView();
+        }
+
+        async void InitWebView()
+        {
+            await webView.EnsureCoreWebView2Async(null);
+            webView.CoreWebView2.Navigate(Path.Combine("file:", Application.StartupPath, "html", "index.html"));
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webView.CoreWebView2.ExecuteScriptAsync("document.getElementById('headerText').style.color = 'blue'");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webView.CoreWebView2.ExecuteScriptAsync("document.getElementById('headerText').innerText = 'HELLO WEBVIEW2!!!'");
+        }
+    }
+}
